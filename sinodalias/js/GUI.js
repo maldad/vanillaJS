2a916b55c5a01afed57ca5f8b5e3926c75083715
maldad <canond_7@hotmail.com>
2017-10-12 18:27:29 -0500

    finishing the design of the form

--- sinodalias/js/GUI.js
+++ sinodalias/js/GUI.js
@@ -4,43 +4,101 @@ var GUI = (function (){
     return document.getElementById(id);
   };
 
-  var _table = function(rows, columns){
-    var table = HTML.new_element("table").element();
-    var tbody = HTML.new_element("tbody").element();
-    for(var i = 0; i < rows; i++){
-      var tr = HTML.new_element("tr").element();
-      for(var i = 0; i < columns; i++){
-        tr.appendChild(HTML.new_element("td").setText(i).element());
-      }
-      tbody.appendChild(tr);
-    }
-    table.appendChild(tbody);
-    return table;
+  var period = function(){
+    var months = HTML.newElement("h3").setText("AGO - DIC 2017");
+    var cont = HTML.newElement("div").addClass("pure-u-1-3").appendChild(months.element());
+    return cont;
   };
 
+  var sep = function(){
+    var img = HTML.newElement("img").setAttribute("src", "img/sep.jpg").
+      setAttribute("width", 300).setAttribute("height", 150);
+    var cont = HTML.newElement("div").addClass("pure-u-1-3").appendChild(img.element());
+    return cont;
+  };
+
+  var ito = function(){
+    var img = HTML.newElement("img").setAttribute("src", "img/ito.png").
+      setAttribute("width", 100).setAttribute("height", 100);
+    var cont = HTML.newElement("div").addClass("pure-u-1-3").appendChild(img.element());
+    return cont;
+  }
+
   var _head = function(){
-    var header = HTML.new_element("div").element();
-    var table = _table(1, 3);
-    var table = HTML.envolver(table);
-    table.setId("table1");
+    var cont = HTML.newElement("div").addClass("pure-g");
+    cont.appendChild(period().element());
+    cont.appendChild(sep().element());
+    cont.appendChild(ito().element());
+    
+    return cont;
+  };
 
-    var period = HTML.envolver(table.element().children[0].children[0].children[0]);
-    period.setText("AGO - DIC 2017");
+  var _form = function(){
+    var form = HTML.newElement("form")
+      .addClass("pure-form");
 
-    var sep = HTML.envolver(table.element().children[0].children[0].children[1]);
-    sep.setText("SEP logo");
+    form.appendChild(HTML.newElement("input")
+        .addClass("pure-input-1-2")
+        .setAttribute("placeholder", "Fecha:").element());
 
-    var ito = HTML.envolver(table.element().children[0].children[0].children[2]);
-    ito.setText("ITO logo");
+    form.appendChild(HTML.newElement("input")
+        .addClass("pure-input-1-2")
+        .setAttribute("placeholder", "Folio:").element());
 
-    header.appendChild(table.element());
+    form.appendChild(HTML.newElement("input")
+        .addClass("pure-input-3-4")
+        .setAttribute("placeholder", "NOMBRE").element());
+
+    form.appendChild(HTML.newElement("input")
+        .addClass("pure-input-1-4")
+        .setAttribute("placeholder", "Matrícula").element());
+
+    form.appendChild(HTML.newElement("input")
+        .addClass("pure-input-1")
+        .setAttribute("placeholder", "CARRERA").element());
+
+    form.appendChild(HTML.newElement("input")
+        .addClass("pure-input-1")
+        .setAttribute("value", "Concepto:                 Examen de titulación Sinodalias")
+        .setAttribute("readonly", "").element());
+
+    form.appendChild(HTML.newElement("input")
+        .addClass("pure-input-1-6")
+        .setAttribute("value", "Nivel: ")
+        .setAttribute("readonly", "").element());
+
+    form.appendChild(HTML.newElement("select")
+        .addClass("pure-input-2-3")
+        .appendChild(HTML.newElement("option").setText("Licenciatura").element())
+        .appendChild(HTML.newElement("option").setText("Maestría").element())  
+        .appendChild(HTML.newElement("option").setText("Doctorado").element())  
+        .element());
+
+    form.appendChild(HTML.newElement("input")
+        .addClass("pure-input-1-6")
+        .setAttribute("value", "Importe  $: ")
+        .setAttribute("readonly", "").element());
+
+    form.appendChild(HTML.newElement("input")
+        .addClass("pure-input-1-3")
+        .setAttribute("placeholder", "Cantidad con número").element());
+
+    form.appendChild(HTML.newElement("input")
+        .addClass("pure-input-1-3")
+        .setAttribute("placeholder", "(Cantidad con letra)").element());
+
+    return form;
+  };
+
+  var _createGui = function(){
     var panel = _getElement("mainPanel");
-    panel.appendChild(header);
+    panel.appendChild(_head().element());
+    panel.appendChild(_form().element());
   };
 
   return{
     "getElement" : _getElement,
-    "head" : _head
+    "createGui" : _createGui
   };
 
 })();
--- sinodalias/js/lib/HTML.js
+++ sinodalias/js/lib/HTML.js
@@ -1,45 +1,42 @@
 var HTML = (function (){
 
-    var _envolver = function(obj){
-        var _estado = obj;
-        var _envoltura = {
-            'setId': function(_id){
-                _estado.setAttribute('id', _id);
-                return _envoltura;
+    var _wrap = function(obj){
+        var _status = obj;
+        var _wrapper = {
+            "setId": function(_id){
+                _status.setAttribute('id', _id);
+                return _wrapper;
             },
-            'setText': function(_text){
-                _estado.textContent = _text;
-                return _envoltura;
+            "setText": function(_text){
+                _status.textContent = _text;
+                return _wrapper;
             },
-            'setAttribute': function(_attribute, value){
-                _estado.setAttribute(_attribute, value);
-                return _envoltura;
+            "setAttribute": function(_attribute, value){
+                _status.setAttribute(_attribute, value);
+                return _wrapper;
             },
-            'addClass': function(_class){
-                _estado.classList.add(_class);
-                return _envoltura;
+            "addClass": function(_class){
+                _status.classList.add(_class);
+                return _wrapper;
             },
-            'appendChild': function(_child){
-                _estado.appendChild(_child);
-                return _envoltura;
+            "appendChild": function(_child){
+                _status.appendChild(_child);
+                return _wrapper;
             },
-            'element': function(){
-                return _estado;
+            "element": function(){
+                return _status;
             },
         };
-        return _envoltura;
+        return _wrapper;
     };
 
-    var _new_element = function(_etiqueta, _obj_atributos){
-        var nuevo_elemento = document.createElement(_etiqueta);
-        for(var atributo in  _obj_atributos)
-            nuevo_elemento.setAttribute(atributo, _obj_atributos[atributo]);
-        return _envolver(nuevo_elemento);
+    var _newElement = function(_tag){
+        var newElement = document.createElement(_tag);
+        return _wrap(newElement);
     };
-
 
     return {
-        'new_element': _new_element,
-        "envolver" : _envolver
+        "newElement": _newElement,
+        "wrap" : _wrap
     };
 })();
